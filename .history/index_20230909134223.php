a31832a757f627ceddff39033ef5529db62f271a
haifabilel <125888354+haifabilel@users.noreply.github.com>
2023-09-09 14:11:07 +0200

    body style_20230909134329

--- .history/index_20230909134223.php
+++ .history/index_20230909134223.php
@@ -6,8 +6,7 @@
     <title>Portfolio</title>
     <!-- Font awesome cdn -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <!-- css link -->
-    <link rel="stylesheet" href="css/style.css"
+    
 </head>
 <body>
     
