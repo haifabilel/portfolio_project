24b984eb40a447696ff20b4ccad1dcfb1018fc09
haifabilel <125888354+haifabilel@users.noreply.github.com>
2023-09-09 13:38:05 +0200

    initialiser projet

---
+++ .history/index_20230909132431.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <!-- Font awesome cdn -->
+    https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.cs
+</head>
+<body>
+    
+</body>
+</html>
